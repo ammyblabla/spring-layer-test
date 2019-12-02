e54e949488812aa898c516c935f9e11cfee8f8c7
Natnaree Asavaseri <nasavas@ap.xom.com>
2019-12-02 23:41:24 +0700

    add mapper test

--- src/main/java/com/example/layertest/Product/ProductMapper.java
+++ src/main/java/com/example/layertest/Product/ProductMapper.java
@@ -2,11 +2,15 @@ package com.example.layertest.Product;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper
 public interface ProductMapper {
+
+    ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
+
     List<ProductDTO> toProductDTOs(List<Product> product);
 
     ProductDTO toProductDTO(Product product);
